73dce3c9ed1ed1538a61908dbf68a32414faf815
Nahuel Manero <nahuam00@gmail.com>
2021-05-24 19:15:07 -0300

    Agregue la clase Obra

---
+++ Modelo/Obra.py
@@ -0,0 +1,73 @@
+#from Modelo import Tipo_Entrada
+class Obra():
+    
+    alto = 0
+    ancho = 0
+    codigoSensor = 0
+    descripcion = ""
+    duracionExtendida = 0.0
+    duracionResumida = 0.0
+    fechaCreacion = ""
+    fechaPrimerIngreso = ""
+    nombreObra = ""
+    peso = 0
+    valuacion = 0
+
+    def __init__(self, alto, ancho, codigoSensor, descripcion, duracionExtendida, duracionResumida, fechaCreacion, fechaPrimerIngreso, nombreObra, peso, valuacion):
+        self.alto = alto
+        self.ancho = ancho
+        self.codigoSensor = codigoSensor
+        self.descripcion = descripcion
+        self.duracionExtendida = duracionExtendida
+        self.duracionResumida = duracionResumida
+        self.fechaCreacion = fechaCreacion
+        self.fechaPrimerIngreso = fechaPrimerIngreso
+        self.nombreObra = nombreObra
+        self.peso = peso
+        self.valuacion = valuacion
+   
+    def conocerArtista():
+        pass
+    
+    def conocerCambioEstado():
+        pass
+    
+    def conocerCompra():
+        pass
+    
+    def conocerDonacion():
+        pass
+    
+    def conocerEmpleado():
+        pass
+    
+    def conocerEstilo():
+        pass
+    
+    def conocerPrestamo():
+        pass
+    
+    def conocerPrestamoAMuseo():
+        pass
+    
+    def conocerRestauracion():
+        pass
+    
+    def conocerSectorAsignado():
+        pass
+    
+    def conocerTecnica():
+        pass
+    
+    def conocerTematica():
+        pass
+    
+    def conocerTipoIngreso():
+        pass
+    
+    def crearCambioEstado():
+        pass
+    
+    def getDuracionResumida():
+        pass
+    
