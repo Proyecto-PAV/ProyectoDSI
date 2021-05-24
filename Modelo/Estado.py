435dcd46514fe1a3f8d400108f6904fc5da93090
Guille Gribaudo <80915763+GuilleTG20@users.noreply.github.com>
2021-05-24 18:53:55 -0300

    clases analisis

---
+++ Modelo/Estado.py
@@ -0,0 +1,22 @@
+
+class estado():
+
+    ambito = ''
+    descripcion = ''
+    estadoReserva = 0
+    nombre = ''
+
+    def __init__(self, ambito, descripcion, estadoReserva, nombre):
+        self.ambito = ambito
+        self.descripcion = descripcion
+        self.estadoReserva = estadoReserva
+        self.nombre = nombre
+    
+    def esAmbitoObra(self):
+        pass
+
+    def esPendienteAsigDeposito(self):
+        pass
+
+    def getEstadoReservaConfirmada(self):
+        pass
