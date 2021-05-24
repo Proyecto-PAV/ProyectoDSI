06c9c6de7026d6adc691e2a9bd329aa74b7f703e
felipearias2024 <felipe2arias@hotmail.com>
2021-05-24 14:20:51 -0300

    clases tipoVisita y TipoEntrada listas

---
+++ Modelo/Tipo_visita.py
@@ -0,0 +1,19 @@
+
+
+class TipoVisita():
+
+    nombre = ""
+    tipoVisita = []
+
+    def __init__(self, nombre, tipoVisita):
+        self.nombre = nombre
+        self.tipoVisita = tipoVisita
+
+    def getNombre(self):
+        pass
+
+    def getTipoVisita(self):
+        pass
+
+    def mostrarNombre(self):
+        pass
