84dcca9736b2a258529610daae2f7d9509dde2cf
AleFontao <82983855+AleFontao@users.noreply.github.com>
2021-05-24 18:46:11 -0300

    Clases

---
+++ Modelo/Sede.py
@@ -0,0 +1,48 @@
+class Sede():
+    
+    cantidadMaximaPorGuia = 0
+    cantidadMaximaVisitantes = 0
+    duracionExposicionesVigentes = 0
+    exposicionesVigentes = ''
+    nombre = ''
+    reservaVisita = ''
+    tarifasVigentes = 0
+
+    def __init__(self, cantidadMaximaPorGuia, cantidadMaximaVisitantes,
+    duracionExposicionesVigentes,exposicionesVigentes,nombre,reservaVisita, tarifasVigentes):
+        self.cantidadMaximaPorGuia = cantidadMaximaPorGuia
+        self.cantidadMaximaVisitantes = cantidadMaximaVisitantes
+        self.duracionExposicionesVigentes = duracionExposicionesVigentes
+        self.exposicionesVigentes = exposicionesVigentes
+        self.nombre = nombre
+        self.reservaVisita = reservaVisita
+        self.tarifasVigentes = tarifasVigentes
+
+    def calcularDuracionAExposicionesVigentes(self, duracionExposicionesVigentes):
+        '''for exposicionesVigentes in self.exposicionesVigentes:
+                if (exposicionesVigentes.esVigente() == True):
+                    duracionExposicionesVigentes += exposicionesVigentes.
+                    return duracionExposicionesVigentes
+                else:
+                    return None
+        '''
+
+        pass
+    
+    def conocerColeccion():
+        pass
+    def conocerDeposito():
+        pass
+    def conocerEmpleado():
+        pass
+    def conocerExposicion():
+        pass
+    def conocerHorario():
+        pass
+    #Se sigue con los def
+
+    def getCantidadMaximaVisitantes(self):
+        return self.cantidadMaximaVisitantes
+
+    def getEntradaVendidas(): #sede en ningun momento conoce la cantidad de entradas vendidas
+        pass
---
+++ Modelo/Sesion.py
@@ -0,0 +1,22 @@
+class Sesion():
+    
+    empleadoSesion = ''
+    fechaFin = ''
+    fechaInicio = ''
+    horaFin = ''
+    horaInicio = ''
+
+
+    def __init__(self, empleadoSesion, fechaFin, fechaInicio, horaFin, horaInicio):
+        self.empleadoSesion = empleadoSesion
+        self.fechaFin = fechaFin
+        self.fechaInicio = fechaInicio
+        self.horaFin = horaFin
+        self.horaInicio = horaInicio
+    
+
+    def conocerUsuario(usuario):
+        usuario.getUsuarios()
+
+    def getEmpleadoenSesion(self, empleadoSesion):
+        return empleadoSesion
