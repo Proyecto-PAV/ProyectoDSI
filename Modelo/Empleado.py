1b6aa59eaf1397d64b61a7420e8a6a9ab8a87377
Luciano Guzman <lucianoguzman24@gmail.com>
2021-05-24 18:10:58 -0300

    Clases
    Clase Entrada y Empleado

---
+++ Modelo/Empleado.py
@@ -0,0 +1,19 @@
+class Empleado():
+
+    nombre = ""
+    apellido = ""
+    codigoValidacion = 0
+    cuit = 00000000000
+    dni = 00000000
+    domicilio = ""
+    fechaDeIngreso = 00/00/0000
+    mail = ""
+    sedeDondeTrabaja = ""
+    sexo = 
+
+    def __init__(self, nombre, apellido, codValidacion, cuit, dni, domicilio, fechDeIngreso, fechaNacimiento, mail, sedeDondeTrabaja, sexo, telefono):
+        self.nombre = nombre,
+        self.tipoEntrada = tipoEntrada
+
+    def getNombre(self):
+        pass
---
+++ Modelo/Entrada.py
@@ -0,0 +1,13 @@
+class Entrada():
+
+    fechaVenta = 00/00/0000
+    horaVenta = 00:00
+    monto = 0
+    numero = 0
+    
+    def __init__(self, fechaVenta, horaVenta, monto, numero):
+        self.nombre = nombre,
+        self.tipoEntrada = tipoEntrada
+
+    def getNombre(self):
+        pass
