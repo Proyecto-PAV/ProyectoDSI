72030a70f17c3c7b2d3d374d90e15a83111979d2
Nahuel Manero <nahuam00@gmail.com>
2021-06-02 20:20:09 -0300

    Hice los metodos de la clase Tarifa y algunos de la Obra

--- Modelo/Tarifa.py
+++ Modelo/Tarifa.py
@@ -1,28 +1,37 @@
 from Modelo import Tipo_Entrada
+from Modelo import Tipo_visita
 class Tarifa():
     
     fechaFinVigencia = ""
-    fechaInicioVifencia = ""
+    fechaInicioVigencia = ""
     monto = 0
     montoAdicionalGuia = 0
 
     def __init__(self, fechaFinVigencia, fechaInicioVifencia, monto, montoAdicionalGuia):
         self.fechaFinVigencia = fechaFinVigencia
-        self.fechaInicioVifencia = fechaInicioVifencia
+        self.fechaInicioVigencia = fechaInicioVigencia
         self.monto = monto
         self.montoAdicionalGuia = montoAdicionalGuia
-   
-    def conocerTipoEntrada(self):
-        pass
     
-    def conocerTipoVisita(self):
-        pass
     
-    def esVigente(self):
-        pass
+    def conocerTipoEntrada(self, entrada):
+        
+        Tipo_Entrada.getTipoEntrada(entrada)
+    
+    
+    def conocerTipoVisita(self, visita):
+        
+        Tipo_visita.getTipoVisita(visita)
+    
+    def esVigente(self, fecha):
+        
+        if fecha <= fechaFinVigencia and fecha >= fechaInicioVigencia:
+            print ("La fecha esta en vigencia")
+        else:
+            print ("La fecha ingresada esta fuera de vigencia")
     
     def getAdicionalPorGuia(self):
-        pass
+        return self.montoAdicionalGuia
     
     def getMonto(self):
-        pass
+        return self.monto
