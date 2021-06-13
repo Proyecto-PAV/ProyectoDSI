5932429ceee949d776509b7573e8a6426e9a48df
Genaro Lambertucci <83099@sistemas.frc.utn.edu.ar>
2021-06-13 19:12:00 -0300

    coenxion BD

--- Base de Datos/CapaConexion.py
+++ Base de Datos/CapaConexion.py
@@ -9,13 +9,22 @@ def conexion():
     password = 'tpaimanero1.'
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
                         server+';DATABASE='+database+';UID='+username+';PWD=' + password)
+    
+    return cnxn
 
 
 class Sede: 
 
     def ObtenerTodasLasSedes(self):
-        com = self.conexion()
+        cnxn = self.conexion()
+        cursor = cnxn.cursor()
         cursor.execute('select * from sedes')
         for row in cursor:
             print(row)
-        
+
+
+    def ObtenerSesionActiva():
+        pass
+
+
+Sede.ObtenerTodasLasSedes()
